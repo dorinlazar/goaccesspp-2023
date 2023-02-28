98830f6694745fda6ef00490f85d41665d7b5a58
Dorin Lazăr (Fedora WSL2) <dorin.lazar@gmail.com>
2023-02-28 20:47:24 +0200

    more compiles

--- src/gmenu.h
+++ src/gmenu.h
@@ -1,27 +1,20 @@
 #pragma once
 #include <ncursesw/ncurses.h>
 
-#ifndef GMENU_H_INCLUDED
-#define GMENU_H_INCLUDED
-
-enum ACTION {
-  REQ_DOWN,
-  REQ_UP,
-  REQ_SEL
-};
+enum ACTION { REQ_DOWN, REQ_UP, REQ_SEL };
 
 typedef struct GMenu_ GMenu;
 typedef struct GItem_ GItem;
 
 /* Menu Item */
 struct GItem_ {
-  char *name;
+  char* name;
   int checked;
 };
 
 /* Menu Panel */
 struct GMenu_ {
-  WINDOW *win;
+  WINDOW* win;
 
   int count;
   int size;
@@ -34,9 +27,9 @@ struct GMenu_ {
   unsigned short multiple;
   unsigned short selectable;
   unsigned short status;
-  GItem *items;
+  GItem* items;
 };
 
-GMenu *new_gmenu (WINDOW * parent, int h, int w, int y, int x);
-int post_gmenu (GMenu * menu);
-void gmenu_driver (GMenu * menu, int c);
+GMenu* new_gmenu(WINDOW* parent, int h, int w, int y, int x);
+int post_gmenu(GMenu* menu);
+void gmenu_driver(GMenu* menu, int c);
